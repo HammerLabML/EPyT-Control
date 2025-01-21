fa2e34cffcf84bc8d3d0213c6e0a6d667ab3526d
André Artelt <aartelt@techfak.de>
2025-01-21 08:05:34 +0100

    Add examples

---
+++ examples/chlorine_control_env.py
@@ -0,0 +1,130 @@
+"""
+This file contains a simple (single specied EPANET) environment for
+controlling the chlorine injection.
+"""
+import os
+import time
+import numpy as np
+from epyt_flow.data.benchmarks import load_leakdb_scenarios
+from epyt_flow.utils import get_temp_folder
+from epyt_flow.simulation import ScenarioSimulator, ToolkitConstants, ModelUncertainty, \
+    ScenarioConfig, ScadaData, SensorConfig
+from epyt_flow.uncertainty import RelativeUniformUncertainty, AbsoluteGaussianUncertainty
+from epyt_control.envs import HydraulicControlEnv
+from epyt_control.actions import ChemicalInjectionAction
+from stable_baselines3 import PPO
+from gymnasium.wrappers import NormalizeObservation
+
+
+def create_scenario() -> ScenarioConfig:
+    # Create scenario based on the LeakDB Hanoi
+    [scenario_config] = load_leakdb_scenarios(scenarios_id=list(range(1)), use_net1=False)
+    with ScenarioSimulator(scenario_config=scenario_config) as sim:
+        # Enable chlorine simulation and place a chlorine injection pump at the reservoir
+        sim.enable_chemical_analysis()
+
+        reservoid_node_id, = sim.epanet_api.getNodeReservoirNameID()
+        sim.add_quality_source(node_id=reservoid_node_id,
+                               pattern=np.array([1.]),
+                               source_type=ToolkitConstants.EN_MASS,
+                               pattern_id="my-chl-injection")
+
+        # Set initial concentration and simple (constant) reactions
+        zero_nodes = [0] * sim.epanet_api.getNodeCount()
+        sim.epanet_api.setNodeInitialQuality(zero_nodes)
+        sim.epanet_api.setLinkBulkReactionCoeff([-.5] * sim.epanet_api.getLinkCount())
+        sim.epanet_api.setLinkWallReactionCoeff([-.01] * sim.epanet_api.getLinkCount())
+
+        # Set flow and chlorine sensors everywhere
+        sim.sensor_config = SensorConfig.create_empty_sensor_config(sim.sensor_config)
+        sim.set_flow_sensors(sim.sensor_config.links)
+
+        # Specify uncertainties -- similar to the one already implemented in LeakDB
+        my_uncertainties = {"global_pipe_length_uncertainty": RelativeUniformUncertainty(low=0, high=0.25),
+                            "global_pipe_roughness_uncertainty": RelativeUniformUncertainty(low=0, high=0.25),
+                            "global_base_demand_uncertainty": RelativeUniformUncertainty(low=0, high=0.25),
+                            "global_demand_pattern_uncertainty": AbsoluteGaussianUncertainty(mean=0, scale=.2)}
+        sim.set_model_uncertainty(ModelUncertainty(**my_uncertainties))
+
+        sim.save_to_epanet_file(os.path.join(get_temp_folder(), f"SimpleChlorineInjectionEnv-{time.time()}.inp"))
+        return sim.get_scenario_config()
+
+
+class SimpleChlorineInjectionEnv(HydraulicControlEnv):
+    """
+    A simple environment for controlling the chlorine injection.
+    """
+    def __init__(self):
+        scenario_config = create_scenario()
+
+        chlorine_injection_action_space = ChemicalInjectionAction(node_id="1",
+                                                                  pattern_id="my-chl-injection",
+                                                                  source_type_id=ToolkitConstants.EN_MASS,
+                                                                  upper_bound=10000.)
+
+        super().__init__(scenario_config=scenario_config,
+                         chemical_injection_actions=[chlorine_injection_action_space],
+                         autoreset=True,
+                         reload_scenario_when_reset=False)
+
+    def _compute_reward_function(self, scada_data: ScadaData) -> float:
+        """
+        Computes the current reward based on the current sensors readings (i.e. SCADA data).
+
+        Parameters
+        ----------
+        :class:`epyt_flow.simulation.ScadaData`
+            Current sensor readings.
+
+        Returns
+        -------
+        `float`
+            Current reward.
+        """
+        new_sensor_config = scada_data.sensor_config
+        new_sensor_config.quality_node_sensors = scada_data.sensor_config.nodes
+        scada_data.change_sensor_config(new_sensor_config)
+
+        # Regulation Limits
+        upper_cl_bound = 2.  # (mg/l)
+        lower_cl_bound = .3  # (mg/l)
+
+        # Sum up (negative) residuals for out of bounds Cl concentrations at nodes -- i.e.
+        # reward of zero means everythings is okay, while a negative reward
+        # denotes Cl concentration bound violations
+        reward = 0
+
+        nodes_quality = scada_data.get_data_nodes_quality()
+
+        upper_bound_violation_idx = nodes_quality > upper_cl_bound
+        reward += -1. * np.sum(nodes_quality[upper_bound_violation_idx] - upper_cl_bound)
+
+        lower_bound_violation_idx = nodes_quality < lower_cl_bound
+        reward += np.sum(nodes_quality[lower_bound_violation_idx] - lower_cl_bound)
+
+        return reward
+
+
+if __name__ == "__main__":
+    with SimpleChlorineInjectionEnv() as env:
+        # Wrap environment
+        env = NormalizeObservation(env)
+
+        # Apply a simple policy learner
+        # You might want to add more wrappers (e.g. normalizing inputs, rewards, etc.) and logging here
+        # Also, inceasing the number of time steps might help as well
+        model = PPO("MlpPolicy", env, verbose=1)
+        model.learn(total_timesteps=1000)
+        model.save("my_model_clinject.zip")
+
+        """
+        # Run some iterations -- note that autorest=True
+        obs, _ = env.reset()
+        for _ in range(20):
+            # TODO: RL logic goes here
+            act = env.action_space.sample()
+            obs, reward, terminated, _, _ = env.step(act)
+
+            #print(obs)
+            print(reward)
+        """
---
+++ examples/pump_control_env.py
@@ -0,0 +1,125 @@
+"""
+This file contains an example of a continous pump speed control environment.
+"""
+import numpy as np
+import pandas as pd
+from epyt_flow.simulation import ScenarioSimulator, ScenarioConfig, ScadaData
+from epyt_control.envs import HydraulicControlEnv
+from epyt_control.actions import PumpSpeedAction
+from stable_baselines3 import SAC
+from gymnasium.wrappers import RescaleAction, NormalizeObservation
+
+
+def create_scenario(f_inp_in: str) -> tuple[ScenarioConfig, list[str]]:
+    """
+    Creates a new scenario for a given .inp file.
+    Note that pressure sensors are placed at every junction.
+    """
+    with ScenarioSimulator(f_inp_in=f_inp_in) as scenario:
+        # Sensors = input to the agent (control strategy)
+        # Place pressure sensors at all junctions
+        junctions = scenario.sensor_config.nodes
+        for tank_id in scenario.sensor_config.tanks:
+            junctions.remove(tank_id)
+        scenario.set_pressure_sensors(sensor_locations=junctions)
+
+        # Place pump efficiency sensors at every pump
+        scenario.place_pump_efficiency_sensors_everywhere()
+
+        # Place flow sensors at every pump and tank connection
+        topo = scenario.get_topology()
+        tank_connections = []
+        for tank in topo.get_all_tanks():
+            for link, _ in topo.get_adjacent_links(tank):
+                tank_connections.append(link)
+
+        flow_sensors = tank_connections + scenario.sensor_config.pumps
+        scenario.set_flow_sensors(flow_sensors)
+
+        # Return the scenario config and tank connections
+        return scenario.get_scenario_config(), tank_connections
+
+
+class ContinuousPumpControlEnv(HydraulicControlEnv):
+    """
+    Class implementing a continous pump speed environment --
+    i.e. a continous action space for the pump speed.
+    """
+    def __init__(self):
+        f_inp_in = "Anytown.inp"
+        scenario_config, tank_connections = create_scenario(f_inp_in)
+
+        self._tank_connections = tank_connections
+        self._network_constraints = {"min_pressure": 28.1227832,
+                                     "max_pressure": 70,
+                                     "max_pump_efficiencies": pd.Series({"b1": .65,
+                                                                         "b2": .65,
+                                                                         "b3": .65})}
+        self._objective_weights = {"pressure_violation": .9,
+                                   "abs_tank_flow": .02,
+                                   "pump_efficiency": .08}
+
+        super().__init__(scenario_config=scenario_config,
+                         pumps_speed_actions=[PumpSpeedAction(pump_id=p_id,
+                                                              speed_upper_bound=4.0)
+                                              for p_id in scenario_config.sensor_config.pumps],
+                         autoreset=True,
+                         reload_scenario_when_reset=False)
+
+    def _compute_reward_function(self, scada_data: ScadaData) -> float:
+        # Compute different objectives and final reward
+        pressure_data = scada_data.get_data_pressures()
+        tanks_flow_data = scada_data.get_data_flows(sensor_locations=self._tank_connections)
+        pumps_flow_data = scada_data.get_data_flows(sensor_locations=scada_data.sensor_config.pumps)
+        pump_efficiency = scada_data.get_data_pumps_efficiency()
+
+        pressure_violations = np.logical_or(
+            pressure_data > self._network_constraints["max_pressure"],
+            pressure_data < self._network_constraints["min_pressure"]
+        ).any(axis=0).sum()
+        n_sensors = pressure_data.shape[1]
+        pressure_obj = float(1 - pressure_violations / n_sensors)
+
+        total_abs_tank_flow = np.abs(tanks_flow_data).sum(axis=None)
+        total_pump_flow = pumps_flow_data.sum(axis=None)
+        tank_obj = float(total_pump_flow / (total_pump_flow + total_abs_tank_flow))
+
+        pump_efficiencies = pd.Series(
+            pump_efficiency.mean(axis=0),
+            index=scada_data.sensor_config.pumps
+        )
+        max_pump_efficiencies = self._network_constraints["max_pump_efficiencies"]
+        normalized_pump_efficiencies = pump_efficiencies / max_pump_efficiencies
+        pump_efficiency_obj = normalized_pump_efficiencies.mean()
+
+        reward = self._objective_weights["pressure_violation"] * pressure_obj + \
+            self._objective_weights["abs_tank_flow"] * tank_obj + \
+            self._objective_weights["pump_efficiency"] * pump_efficiency_obj
+
+        return reward
+
+
+if __name__ == "__main__":
+    with ContinuousPumpControlEnv() as env:
+        # Wrap environment
+        env = NormalizeObservation(env)
+        env = RescaleAction(env, min_action=-1, max_action=1)
+
+        # Apply a simple policy learner
+        # You might want to add wrappers (e.g. normalizing inputs, rewards, etc.) and logging here
+        # Also, inceasing the number of time steps might help as well
+        model = SAC("MlpPolicy", env)
+        model.learn(total_timesteps=1000)
+        model.save("my_model_pumpspeed.zip")
+
+        """
+        # Run some iterations -- note that autorest=True
+        obs, _ = env.reset()
+        for _ in range(20):
+            # TODO: RL logic goes here
+            act = env.action_space.sample()
+            obs, reward, terminated, _, _ = env.step(act)
+
+            #print(obs)
+            print(reward)
+        """
