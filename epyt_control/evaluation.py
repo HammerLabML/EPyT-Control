11631e41b88b22a9eaf7fb51ca1c4b22034d6304
André Artelt <aartelt@techfak.de>
2025-01-31 09:09:12 +0100

    Evaluation func: Compatibility with wrapped envs

--- epyt_control/evaluation.py
+++ epyt_control/evaluation.py
@@ -1,24 +1,31 @@
 """
 This module provides functions for evaluating policies/agents/control strategies on environments.
 """
-from typing import Callable
+from typing import Callable, Union
 import numpy as np
 from epyt_flow.simulation import ScadaData
+from gymnasium import Wrapper
 
 from .envs import RlEnv
 
 
-def evaluate_policy(env: RlEnv, policy: Callable[[np.ndarray], np.ndarray],
-                    n_max_iter: int = 1) -> tuple[list[float], ScadaData]:
+def evaluate_policy(env: Union[RlEnv, Wrapper], policy: Callable[[np.ndarray], np.ndarray],
+                    n_max_iter: int = 10) -> tuple[list[float], ScadaData]:
     """
     Evaluates a given policy/agent/control strategy for a given environment --
-    i.e. the policy/agent is applied to the environment and the rewards and ScadaData observations
-    over time are recorded.
+    i.e. the policy/agent is applied to the environment and the rewards and
+    `ScadaData <https://epyt-flow.readthedocs.io/en/stable/epyt_flow.simulation.scada.html#epyt_flow.simulation.scada.scada_data.ScadaData>`_
+    observations over time are recorded.
 
     Parameters
     ----------
-    env : :class:`~epyt_control.envs.rl_env.RlEnv`
-        The environment -- note that 'autoreset' must be set to False.
+    env : :class:`~epyt_control.envs.rl_env.RlEnv` or `gymnasium.Wrapper <https://gymnasium.farama.org/api/wrappers/#gymnasium.Wrapper>`_
+        The environment.
+
+        Note that in the case of a
+        `gymnasium.Wrapper <https://gymnasium.farama.org/api/wrappers/#gymnasium.Wrapper>`_
+        instance, the underlying environment must be an instance of
+        :class:`~epyt_control.envs.rl_env.RlEnv`.
     policy : `Callable[[numpy.ndarray], numpy.ndarray]`
         Policy/Agent/Control strategy to be evaluated.
     n_max_iter : `int`, optional
@@ -33,9 +40,13 @@ def evaluate_policy(env: RlEnv, policy: Callable[[np.ndarray], np.ndarray],
         `epyt_flow.simulation.ScadaData <https://epyt-flow.readthedocs.io/en/stable/epyt_flow.simulation.scada.html#epyt_flow.simulation.scada.scada_data.ScadaData>`_
         instance containing the WDN states over time.
     """
-    if not isinstance(env, RlEnv):
-        raise TypeError("'env' must be an instance of 'epyt_contro.envs.RlEnv' " +
-                        f"but not of '{type(env)}'")
+    if not isinstance(env, RlEnv) and not isinstance(env, Wrapper):
+        raise TypeError("'env' must be an instance of 'epyt_control.envs.RlEnv' or " +
+                        f"'gymnasium.Wrapper' but not of '{type(env)}'")
+    if isinstance(env, Wrapper):
+        if not isinstance(env.env, RlEnv):
+            raise TypeError("The wrapped environment must be an insance of " +
+                            f"'epyt_control.envs.RlEnv' but not of '{type(env.env)}'")
     if not callable(policy):
         raise TypeError("'policy' must be callable -- " +
                         "i.e. mapping observations (numpy.ndarray) to actions (numpy.ndarray)")
